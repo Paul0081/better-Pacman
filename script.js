5e276e42bca977d11495380e66ab3dc3aa634231
Paul0081 <pgryszczyk@gmail.com>
2022-11-05 17:18:34 +0100

    Dodanie postaci Pacman

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ function hide_showElm(){
     document.getElementById("btnStart").style.display = "none";
     document.getElementById("hideCanvas").style.display = "inline-block";
 }
+const canvas = document.getElementById("canvasMap");
+const ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.arc(200, 100, 22, Math.PI / 7, -Math.PI / 7, false);
+ctx.lineTo(195, 100);
+ctx.fillStyle = "yellow";
+ctx.fill();
+ctx.closePath();
+
