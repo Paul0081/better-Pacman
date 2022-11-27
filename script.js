87b69bb97baf97477e68f6e4ddbe71dfb1fbb186
Vexmeritmen <smyrek.nathan@gmail.com>
2022-11-27 16:01:50 +0100

    Dodanie pierwszego przeciwnika Pacmana, który losowo się porusza.

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ function hide_showElm(){
 
 
 
-
-
 var ctx = null;
 var gameMap = [
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -57,7 +55,56 @@ Character.prototype.placeAt = function(x, y)
     this.position	= [((tileW*x)+((tileW-this.dimensions[0])/2)),
         ((tileH*y)+((tileH-this.dimensions[1])/2))];
 };
+var AI = new Ghost();
+
+function Ghost()
+{
+    this.tileFrom	= [1,1];
+    this.tileTo		= [1,1];
+    this.timeMoved	= 0;
+    this.dimensions	= [0,0];
+    this.position	= [800,350];
+    this.delayMove	= 200;
+}
+Ghost.prototype.placeAt = function(x, y)
+{
+    this.tileFrom	= [x,y];
+    this.tileTo		= [x,y];
+    this.position	= [((tileW*x)+((tileW-this.dimensions[0])/2)),
+        ((tileH*y)+((tileH-this.dimensions[1])/2))];
+};
+
 Character.prototype.processMovement = function(t)
+{
+    if(this.tileFrom[0]==this.tileTo[0] && this.tileFrom[1]==this.tileTo[1]) { return false; }
+
+    if((t-this.timeMoved)>=this.delayMove)
+    {
+        this.placeAt(this.tileTo[0], this.tileTo[1]);
+    }
+    else
+    {
+        this.position[0] = (this.tileFrom[0] * tileW) + ((tileW-this.dimensions[0])/2);
+        this.position[1] = (this.tileFrom[1] * tileH) + ((tileH-this.dimensions[1])/2);
+
+        if(this.tileTo[0] != this.tileFrom[0])
+        {
+            var diff = (tileW / this.delayMove) * (t-this.timeMoved);
+            this.position[0]+= (this.tileTo[0]<this.tileFrom[0] ? 0 - diff : diff);
+        }
+        if(this.tileTo[1] != this.tileFrom[1])
+        {
+            var diff = (tileH / this.delayMove) * (t-this.timeMoved);
+            this.position[1]+= (this.tileTo[1]<this.tileFrom[1] ? 0 - diff : diff);
+        }
+
+        this.position[0] = Math.round(this.position[0]);
+        this.position[1] = Math.round(this.position[1]);
+    }
+
+    return true;
+}
+Ghost.prototype.processMovement = function(t)
 {
     if(this.tileFrom[0]==this.tileTo[0] && this.tileFrom[1]==this.tileTo[1]) { return false; }
 
@@ -134,7 +181,17 @@ function draw()
     if(ctx==null) { return; }
 
     var currentFrameTime = Date.now();
+    let brush = Math.floor((Math.random() * 4) + 1);
+    if(!AI.processMovement(currentFrameTime))
+    {
+        if(brush===1 && AI.tileFrom[1]>0 && gameMap[toIndex(AI.tileFrom[0], AI.tileFrom[1]-1)]==1) { AI.tileTo[1]-= 1; }
+        else if(brush===2 && gameMap[toIndex(AI.tileFrom[0], AI.tileFrom[1]+1)]==1) { AI.tileTo[1]+= 1; }
+        else if(brush===3 && AI.tileFrom[0]>0 && gameMap[toIndex(AI.tileFrom[0]-1, AI.tileFrom[1])]==1) { AI.tileTo[0]-= 1; }
+        else if(brush===4 && AI.tileFrom[0]<(mapW-1) && gameMap[toIndex(AI.tileFrom[0]+1, AI.tileFrom[1])]==1) { AI.tileTo[0]+= 1; }
 
+        if(AI.tileFrom[0]!=AI.tileTo[0] || AI.tileFrom[1]!=AI.tileTo[1])
+        { AI.timeMoved = currentFrameTime; }
+    }
 
     if(!player.processMovement(currentFrameTime))
     {
@@ -146,6 +203,7 @@ function draw()
         if(player.tileFrom[0]!=player.tileTo[0] || player.tileFrom[1]!=player.tileTo[1])
         { player.timeMoved = currentFrameTime; }
     }
+
 
     for(var y = 0; y < mapH; ++y)
     {
@@ -171,4 +229,119 @@ function draw()
     ctx.fill();
     ctx.closePath();
 
+    ctx.beginPath();
+    ctx.arc(AI.position[0], AI.position[1], 16, Math.PI / 7, -Math.PI / 7, false);
+    ctx.lineTo(AI.position[0]-1, AI.position[1]);
+    ctx.fillStyle = "red";
+    ctx.fill();
+    ctx.closePath();
 }
+
+
+/*
+var AI = new Ghost();
+
+function Ghost()
+{
+    this.tileFrom	= [1,1];
+    this.tileTo		= [1,1];
+    this.timeMoved	= 0;
+    this.dimensions	= [0,0];
+    this.position	= [800,350];
+    this.delayMove	= 200;
+}
+Ghost.prototype.placeAt = function(x, y)
+{
+    this.tileFrom	= [x,y];
+    this.tileTo		= [x,y];
+    this.position	= [((tileW*x)+((tileW-this.dimensions[0])/2)),
+        ((tileH*y)+((tileH-this.dimensions[1])/2))];
+};
+
+
+
+Ghost.prototype.processMovement = function(t)
+{
+    if(this.tileFrom[0]==this.tileTo[0] && this.tileFrom[1]==this.tileTo[1]) { return false; }
+
+    if((t-this.timeMoved)>=this.delayMove)
+    {
+        this.placeAt(this.tileTo[0], this.tileTo[1]);
+    }
+    else
+    {
+        this.position[0] = (this.tileFrom[0] * tileW) + ((tileW-this.dimensions[0])/2);
+        this.position[1] = (this.tileFrom[1] * tileH) + ((tileH-this.dimensions[1])/2);
+
+        if(this.tileTo[0] != this.tileFrom[0])
+        {
+            var diff = (tileW / this.delayMove) * (t-this.timeMoved);
+            this.position[0]+= (this.tileTo[0]<this.tileFrom[0] ? 0 - diff : diff);
+        }
+        if(this.tileTo[1] != this.tileFrom[1])
+        {
+            var diff = (tileH / this.delayMove) * (t-this.timeMoved);
+            this.position[1]+= (this.tileTo[1]<this.tileFrom[1] ? 0 - diff : diff);
+        }
+
+        this.position[0] = Math.round(this.position[0]);
+        this.position[1] = Math.round(this.position[1]);
+    }
+
+    return true;
+}
+
+function toIndex(x, y)
+{
+    return((y * mapW) + x);
+}
+
+window.onload = function()
+{
+    ctx = document.getElementById('canvasMap').getContext("2d");
+
+    setInterval(draw, 10);
+
+};
+
+function draw()
+{
+    if(ctx==null) { return; }
+    var currentFrameTime = Date.now();
+    let brush = Math.floor((Math.random() * 4) + 1);
+    if(!AI.processMovement(currentFrameTime))
+    {
+        if(brush===1 && AI.tileFrom[1]>0 && gameMap[toIndex(AI.tileFrom[0], AI.tileFrom[1]-1)]==1) { AI.tileTo[1]-= 1; }
+        else if(brush===2 && gameMap[toIndex(AI.tileFrom[0], AI.tileFrom[1]+1)]==1) { AI.tileTo[1]+= 1; }
+        else if(brush===3 && AI.tileFrom[0]>0 && gameMap[toIndex(AI.tileFrom[0]-1, AI.tileFrom[1])]==1) { AI.tileTo[0]-= 1; }
+        else if(brush===4 && AI.tileFrom[0]<(mapW-1) && gameMap[toIndex(AI.tileFrom[0]+1, AI.tileFrom[1])]==1) { AI.tileTo[0]+= 1; }
+
+        if(AI.tileFrom[0]!=AI.tileTo[0] || AI.tileFrom[1]!=AI.tileTo[1])
+        { AI.timeMoved = currentFrameTime; }
+    }
+    for(var y = 0; y < mapH; ++y)
+    {
+        for(var x = 0; x < mapW; ++x)
+        {
+            switch(gameMap[((y*mapW)+x)])
+            {
+                case 0:
+                    ctx.fillStyle = "#0031FF";
+                    break;
+                default:
+                    ctx.fillStyle = "#0D1940";
+            }
+
+            ctx.fillRect( x*tileW, y*tileH, tileW, tileH);
+        }
+    }
+
+    ctx.beginPath();
+    ctx.arc(AI.position[0], AI.position[1], 16, Math.PI / 7, -Math.PI / 7, false);
+    ctx.lineTo(AI.position[0]-1, AI.position[1]);
+    ctx.fillStyle = "red";
+    ctx.fill();
+    ctx.closePath();
+
+}
+*/
